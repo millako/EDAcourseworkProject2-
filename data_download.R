ee76cb26a5688c689b4aac826d21a376510d194c
Milla Korzhuk <ludmillakorzhuk@yahoo.com>
2016-08-20 21:24:32 -0500

    Add files via upload

---
+++ data_download.R
@@ -0,0 +1,17 @@
+# Download the file and set work derictory if necessary (29MB)
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+filePath <- "./data/exdata%2Fdata%2FNEI_data.zip"
+
+if (!file.exists("data")) {
+        dir.create("data")
+}
+
+if (!file.exists(filePath)) {
+        download.file(fileUrl, destfile = filePath)
+        dateDownloaded <- date()
+        dateDownloaded
+        
+        # And unzip it
+        unzip(filePath, exdir="./data")
+}
+dir()
